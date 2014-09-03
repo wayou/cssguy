8442d2c55e4c6895226e0b569c9cf354acb6e79d
Wayou Liu <liuwayong@gmail.com>
2014-09-03 20:53:32 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//refernce :http://eminkura.com/blog/2012/03/04/following-eyes-with-html5-canvas/
+
+var guy1 = new Guy( {
+     'appendElement': document.getElementById( 'bear1' )
+} );
+
+var guy2 = new Guy( {
+     'appendElement': document.getElementById( 'bear2' )
+} );
